6a886dda790905329c6d3ddcac8c9822b40a4a96
phamdaiyb98 <44748066+phamdaiyb98@users.noreply.github.com>
2019-06-27 21:05:25 +0700

    updateDatabase

--- Database/hotel-management.sql
+++ Database/hotel-management.sql
@@ -50,7 +50,7 @@ create table zone(
 create table chamber(
 	chamber_id bigint auto_increment,
     chamber_number varchar(10),
-    type varchar(25),
+    chamber_type varchar(25),
     is_vip boolean,
     price_day varchar(25),
     chamber_area varchar(25),
@@ -63,12 +63,12 @@ create table chamber(
 );
 create table furniture(
 	furniture_id bigint auto_increment,
-    name varchar(125),
+    furniture_name varchar(125),
     constraint furniture_pk primary key (furniture_id)
 );
 create table chamber_furniture(
 	chamber_furniture_id bigint auto_increment,
-    status varchar(125),
+    chamber_furniture_status varchar(125),
     note varchar(255),
     furniture_id bigint,
     chamber_id bigint,
@@ -78,7 +78,7 @@ create table chamber_furniture(
 );
 create table guest(
 	guest_id bigint auto_increment,
-    name varchar(125),
+    guest_name varchar(125),
     birth date,
     id_cart varchar(25),
     passport varchar(25),
@@ -96,9 +96,9 @@ create table desk(
 );
 create table food_item(
 	food_item_id bigint auto_increment,
-    name varchar(50),
+    food_item_name varchar(50),
 	category varchar(50),
-    description varchar(125),
+    food_item_description varchar(125),
     price varchar(25),
     image varchar(255),
     constraint food_item_pk primary key (food_item_id)
@@ -131,9 +131,9 @@ create table order_food_desk(
 );
 create table service(
 	service_id bigint auto_increment,
-    name varchar(125),
+    service_name varchar(125),
     price varchar(25),
-    description varchar(125),
+    service_description varchar(125),
     discount varbinary(25),
     constraint service_pk primary key (service_id)
 );
@@ -170,17 +170,17 @@ create table rental(
 );
 create table section(
 	section_id bigint auto_increment,
-    name varchar(50),
+    section_name varchar(50),
     section_manager_id bigint,
     constraint section_pk primary key (section_id)
 );
 create table employee(
 	employee_id bigint auto_increment,
-    name varchar(125),
+    employee_name varchar(125),
     birth date,
     gender varchar(10),
     address varchar(125),
-    position varchar(25),
+    employee_position varchar(25),
     email varchar(50),
     phone_number varchar(25),
     salary varchar(25),
@@ -189,9 +189,9 @@ create table employee(
     constraint employee_pk primary key (employee_id),
     constraint employee_fk foreign key (section_id) references section(section_id)
 );
-create table work(
+create table work_(
 	work_id bigint auto_increment,
-    name varchar(125),
+    work_name varchar(125),
     content varchar(255),
     constraint work_pk primary key (work_id)
 );
@@ -203,7 +203,7 @@ create table employee_work(
     work_id bigint ,
     employee_id bigint,
     constraint employee_work_pk primary key (employee_work_id),
-    constraint employee_work_fk foreign key (work_id) references work(work_id),
+    constraint employee_work_fk foreign key (work_id) references work_(work_id),
     constraint employee_work_fk2 foreign key (employee_id) references employee(employee_id)
 )
 
