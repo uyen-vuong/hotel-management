3e82ed82e2e8f02c4114846e4f16042891cb3613
Hung <hunghaui2310@gmail.com>
2019-06-27 23:10:29 +0700

    login perfect

--- HotelManagement/src/main/java/com/devpro/spring/repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.devpro.spring.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.devpro.spring.model.Student;
-
-public interface StudentRepository extends JpaRepository<Student, Long> {
-
-}
