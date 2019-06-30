9f7af2d0ae0ef91332a4ff0d28b03d716b24d7f8
phamdaiyb98 <44748066+phamdaiyb98@users.noreply.github.com>
2019-06-30 11:04:32 +0700

    edit-database

--- Database/hotel-management.sql
+++ Database/hotel-management.sql
@@ -31,45 +31,45 @@ create table persistent_login(
 );
 create table check_in(
 	check_in_id bigint auto_increment,
-    check_in_date date,
+    check_in_date varchar(25),
     deposit varchar(25),
-    note varchar(500),
+    note nvarchar(500),
     constraint check_in_pk primary key (check_in_id)
 );
 create table check_out(
 	check_out_id bigint auto_increment,
-    check_out_date date,
-    note varchar(500),
+    check_out_date varchar(25),
+    note nvarchar(500),
     constraint check_out_pk primary key (check_out_id)
 );
 create table zone(
 	zone_id bigint auto_increment,
-    zone_name varchar(50),
+    zone_name nvarchar(50),
     constraint zone_pk primary key (zone_id)
 );
 create table chamber(
 	chamber_id bigint auto_increment,
-    chamber_number varchar(10),
-    chamber_type varchar(25),
-    is_vip boolean,
+    chamber_number nvarchar(10),
+    chamber_type nvarchar(25),
+    is_vip nvarchar(10),
     price_day varchar(25),
     chamber_area varchar(25),
     capacity varchar(10),
-    note varchar(255),
-    is_empty boolean not null,
+    note nvarchar(255),
+    is_empty nvarchar(10) not null,
     zone_id bigint,
     constraint chamber_pk primary key (chamber_id),
     constraint chamber_fk foreign key (zone_id) references zone(zone_id)
 );
 create table furniture(
 	furniture_id bigint auto_increment,
-    furniture_name varchar(125),
+    furniture_name nvarchar(125),
     constraint furniture_pk primary key (furniture_id)
 );
 create table chamber_furniture(
 	chamber_furniture_id bigint auto_increment,
-    chamber_furniture_status varchar(125),
-    note varchar(255),
+    chamber_furniture_status nvarchar(125),
+    note nvarchar(255),
     furniture_id bigint,
     chamber_id bigint,
 	constraint chamber_furniture_pk primary key (chamber_furniture_id),
@@ -78,34 +78,34 @@ create table chamber_furniture(
 );
 create table guest(
 	guest_id bigint auto_increment,
-    guest_name varchar(125),
-    birth date,
-    id_cart varchar(25),
+    guest_name nvarchar(125),
+    birth nvarchar(25), 
+    id_card varchar(25),
     passport varchar(25),
-    address varchar(125),
-    nationality varchar(50),
+    address nvarchar(125),
+    nationality nvarchar(50),
     phone_number varchar(25),
-    is_vip boolean,
+    is_vip nvarchar(10),
     constraint guest_pk primary key (guest_id)
 );
 create table desk(
 	desk_id bigint auto_increment,
-    location varchar(25),
-    is_empty boolean,
+    location nvarchar(25),
+    is_empty nvarchar(10),
     constraint desk_pk primary key (desk_id)
 );
 create table food_item(
 	food_item_id bigint auto_increment,
-    food_item_name varchar(50),
-	category varchar(50),
-    food_item_description varchar(125),
+    food_item_name nvarchar(50),
+	category nvarchar(50),
+    food_item_description nvarchar(125),
     price varchar(25),
     image varchar(255),
     constraint food_item_pk primary key (food_item_id)
 );
 create table order_food(
 	order_food_id bigint auto_increment,
-    quantity int,
+    quantity varchar(10),
     total_price varchar(25),
     discount varchar(25),
     guest_id bigint,
@@ -114,7 +114,7 @@ create table order_food(
 );
 create table menu(
 	menu_id bigint auto_increment,
-    quantity int,
+    quantity varchar(10),
     order_food_id bigint,
     food_item_id bigint,
     constraint menu_pk primary key (menu_id),
@@ -131,9 +131,9 @@ create table order_food_desk(
 );
 create table service(
 	service_id bigint auto_increment,
-    service_name varchar(125),
+    service_name nvarchar(125),
     price varchar(25),
-    service_description varchar(125),
+    service_description nvarchar(125),
     discount varbinary(25),
     constraint service_pk primary key (service_id)
 );
@@ -147,15 +147,15 @@ create table guest_service(
 );
 create table payment(
 	payment_id bigint auto_increment,
-    method varchar(50),
-    payment_date date,
+    method nvarchar(50),
+    payment_date varchar(25),
     constraint payment_pk primary key (payment_id)
 );
 create table rental(
 	rental_id bigint auto_increment,
     total_payment varchar(25),
     discount varchar(25),
-    note varchar(255),
+    note nvarchar(255),
     guest_id bigint,
     check_in_id bigint,
     check_out_id bigint,
@@ -170,17 +170,17 @@ create table rental(
 );
 create table section(
 	section_id bigint auto_increment,
-    section_name varchar(50),
+    section_name nvarchar(50),
     section_manager_id bigint,
     constraint section_pk primary key (section_id)
 );
 create table employee(
 	employee_id bigint auto_increment,
     employee_name varchar(125),
-    birth date,
-    gender varchar(10),
-    address varchar(125),
-    employee_position varchar(25),
+    birth varchar(25),
+    gender nvarchar(10),
+    address nvarchar(125),
+    employee_position nvarchar(25),
     email varchar(50),
     phone_number varchar(25),
     salary varchar(25),
@@ -191,21 +191,23 @@ create table employee(
 );
 create table work_(
 	work_id bigint auto_increment,
-    work_name varchar(125),
-    content varchar(255),
+    work_name nvarchar(125),
+    content nvarchar(255),
     constraint work_pk primary key (work_id)
 );
 create table employee_work(
 	employee_work_id bigint auto_increment,
-    start_date date,
+    start_date varchar(25),
     quality varchar(50),
     bonus varchar(10),
-    work_id bigint ,
+    work_id bigint,
     employee_id bigint,
     constraint employee_work_pk primary key (employee_work_id),
     constraint employee_work_fk foreign key (work_id) references work_(work_id),
     constraint employee_work_fk2 foreign key (employee_id) references employee(employee_id)
-)
+);
+insert into App_User (USER_ID, USER_NAME, ENCRYTED_PASSWORD, ENABLED) values (2, 'user', '$2a$10$PrI5Gk9L.tSZiW9FXhTS8O8Mz9E97k2FZbFvGFFaSsiTUIl.TCrFu', 1);
+insert into App_User (USER_ID, USER_NAME, ENCRYTED_PASSWORD, ENABLED) values (3, 'admin', '$2a$10$PrI5Gk9L.tSZiW9FXhTS8O8Mz9E97k2FZbFvGFFaSsiTUIl.TCrFu', 1);
 
 
 
