927b1dcd4f7fee2fbdc494ee0b61cd9e74463418
phamdaiyb98 <44748066+phamdaiyb98@users.noreply.github.com>
2019-07-02 13:57:39 +0700

    quanLyKhach

---
+++ HotelManagement/src/main/java/com/devpro/spring/repository/GuestRepository.java
@@ -0,0 +1,9 @@
+package com.devpro.spring.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devpro.spring.model.Guest;
+
+public interface GuestRepository extends JpaRepository<Guest, Long>{
+	
+}
